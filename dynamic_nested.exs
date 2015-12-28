7e3054cc536f343a8f9fc5440ed41369b11f4c94
Dean Chouinard <deanchouinard@gmail.com>
2015-12-28 09:08:33 -0500

    dynamic nested

---
+++ dynamic_nested.exs
@@ -0,0 +1,17 @@
+nested = %{
+  buttercup: %{
+    actor: %{
+      first: "Robin",
+      last: "Wright"
+    },
+    role: "princess"
+  },
+  westley: %{
+    actor: %{
+      first: "Carey",
+      last: "Eves"
+    },
+    role: "farm boy"
+  }
+}
+
