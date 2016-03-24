8f9c8624cb9f9a7e8db7f444493093c623e6fc3b
Dean Chouinard <deanchouinard@gmail.com>
2016-03-24 18:10:49 -0400

    finished fib

--- fib.exs
+++ fib.exs
@@ -10,7 +10,7 @@ defmodule FibSolver do
     end
   end
 
-  defp fib_calc(0), do: o
+  defp fib_calc(0), do: 0
   defp fib_calc(1), do: 1
   defp fib_calc(n), do: fib_calc(n-1) + fib_calc(n-2)
 end
@@ -45,5 +45,15 @@ end
 
 to_process = [37, 37, 37, 37, 37, 37 ]
 Enum.each 1..10, fn num_processes ->
-  {time, result] = :timer.tc(
-  Scheduler, :rn
+  {time, result} = :timer.tc(
+  Scheduler, :run,
+  [num_processes, FibSolver, :fib, to_process]
+  )
+
+  if num_processes == 1 do
+    IO.puts inspect result
+    IO.puts "\n #   time (s)"
+  end
+  :io.format "~2B       ~.2f~n", [num_processes, time/1000000.0]
+end
+
